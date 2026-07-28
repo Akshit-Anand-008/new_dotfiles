15982d171a1034a64ca020b11fe104596c4a7bd3
Akshit_Anand <akshitanand008@gmail.com>
2026-07-28 23:26:30 +0530

    Commit on 2026-07-28 23:26

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -17,7 +17,7 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = { "make", "tsv" },
     callback = function()
         vim.opt_local.expandtab = false
-        vim.opt_local.shiftwidth = 8
+        vim.opt_local.shiftwidth = 4
     end,
 })
 
